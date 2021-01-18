8477657aec06a09531cf849e473d6d45f0f05759
MarianoIR <marianoir@gmail.com>
2021-01-18 12:27:49 +0100

    update mongo feathers

--- src/mongodb.js
+++ src/mongodb.js
@@ -1,9 +1,9 @@
 const MongoClient = require('mongodb').MongoClient;
 
 module.exports = function (app) {
-  const connection = app.get('mongodb');
+    const connection = app.get('mongodb');
   const database = connection.substr(connection.lastIndexOf('/') + 1);
-  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true })
+  const mongoClient = MongoClient.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(client => client.db(database));
 
   app.set('mongoClient', mongoClient);
